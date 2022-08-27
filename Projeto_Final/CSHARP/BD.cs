a85aa34b05e83334a855d81498b01886b99802f0
Rodrigogmsouza <81395544+Rodrigogmsouza@users.noreply.github.com>
2022-08-26 22:13:04 -0300

    v0.9

---
+++ Projeto_Final/CSHARP/BD.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_C.Classes
+{
+    public class BancoDeDados
+    {
+        private static MySqlConnection conn;
+        private static string server;
+        private static string database;
+        private static string uid;
+        private static string password;
+
+        //Constructor
+        public static void DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private static void Initialize()
+        {
+            //server = "localhost";
+            server = "10.200.119.241";
+            //database = "connectcsharptomysql";
+            database = "nathalia_rodrigo";
+            //uid = "username";
+            uid = "admin";
+            //password = "password";
+            password = "senai";
+            string myConnectionString;
+            myConnectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                //conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //MessageBox.Show(ex.Message);
+            }
+        }
+
+        //open connection to database
+        private static bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+
+        }
+
+        //Close connection
+        private static bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
