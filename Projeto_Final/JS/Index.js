1c2760997dbcef7a4fcb46d3779c412e6fc824a6
Rodrigo <rodrigo.gmssouza@gmail.com>
2022-10-01 15:12:47 -0300

    v4.8

--- Projeto_Final/JS/Index.js
+++ Projeto_Final/JS/Index.js
@@ -54,7 +54,7 @@ function LogOff() {
     data = data.toGMTString();
     // Apaga o cookie
     document.cookie = 'UserLogin=; expires = ' + data + '; path = /';
-    window.location.href = "/HTMLCSS/Index.cshtml";
+    window.location.href = "~/Index.cshtml";
 }
 
 // MUDA SLIDES DA MAIN.CSHTML
